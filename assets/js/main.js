c0793c973103b2724be66999b70cce780db1b1d9
laiozago <laiozago@gmail.com>
2023-01-19 01:52:06 -0300

    :construction: capturei os inputs e buttons

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const loginUsuario = document.querySelector('#login-usuario');//const para pegar o valor do email. lembra de usar o .value
+const loginSenha = document.querySelector('#login-senha'); //const para pegar o valor da senha. lembra de usar o .value
+const btnEntrar = document.querySelector('#botao-entrar')
+const cadastroEmail = document.querySelector('#cadastro-email');
+const cadastroSenha = document.querySelector('#cadastro-senha');
+const cadastroRepeteSenha = document.querySelector('#cadastro-repete-senha');
+const inputDescricao = document.querySelector('#input-descricao');
+const inputDetalhamento = document.querySelector('#input-detalhamento');
+const btnSalvar = document.querySelector('#btn-salvar');
+const btnApagar = document.querySelector('#btn-apagar');
+const btnEditar = document.querySelector('#btn-entrar');
+
