0beef5468956146471822344d6a69bd48b266178
laiozago <laiozago@gmail.com>
2023-01-30 04:10:06 -0300

    :bulb: comentários

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,7 +1,4 @@
-
-//parte do codigo para pagina de cadastro
-
-
+//*parte do codigo para pagina de cadastro
 //variavel para guardar o nome de usuario
 let usuario = document.querySelector('#cadastro-email');
 //variavel para guardar a senha do usuario
@@ -11,15 +8,20 @@ let senhaRepetida = document.querySelector('#cadastro-repete-senha');
 //variavel para o botao
 let botaoCriaConta = document.querySelector('#botao-entrar')
 
+//*funcao para criar conta
 botaoCriaConta.addEventListener("click",function(){
-    
+    //verifica se as senhas sao iguais e se o usuario digitou algo
     if (senha.value === senhaRepetida.value && usuario.value) {
+        //remove a classe review da senha e do usuario
         senhaRepetida.classList.remove('review')
         usuario.classList.remove('review')
+        //salva os dados no localstorage
         localStorage.setItem('login',usuario.value)
         localStorage.setItem('senha', senha.value);
+        //redireciona para a pagina de login
         window.open('index.html',"_self")
     } else if(senha.value!=senhaRepetida.value){
+        //adiciona a classe review na senha e na senha repetida
         senhaRepetida.classList.add('review')
     }else if(!usuario.value){
         usuario.classList.add('review')
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,18 @@
 const btnEntrar = document.querySelector('#botao-entrar');
 
+//* Escuta o evento de click no botão de entrar
 btnEntrar.addEventListener("click", function () {
+    // Pega os valores dos inputs de usuário e senha
     const userName = document.querySelector('#login-usuario');
     const userPassword = document.querySelector('#login-senha');
-
+        // Verifica se o usuário e a senha estão corretos
         if (userName.value == localStorage.getItem('login') && userPassword.value == localStorage.getItem('senha')) {
             window.location.href = "grownotes.html";
-            console.log('é pra abrir');
         } else if (userName.value == localStorage.getItem('login') && !(userPassword.value == localStorage.getItem('senha'))) {
+            // Se o usuário estiver correto, mas a senha estiver incorreta, exibe uma mensagem de erro
             alert("Senha incorreta");
         } else if (!(userName.value == localStorage.getItem('login')) && userPassword.value == localStorage.getItem('senha')) {
+            // Se a senha estiver correta, mas o usuário estiver incorreto, exibe uma mensagem de erro
             alert("Usuário incorreto ou não cadastrado");
         }
 })
